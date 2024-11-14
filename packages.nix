1432d936f4f0d1b3c5be07b58d366bad7cca0a90
Simon Jones <github@simonjjones.com>
2024-11-13 18:57:57 -0700

    fix: add key-value pair to platform.txt if it does not exist

--- packages.nix
+++ packages.nix
@@ -57,13 +57,19 @@ let
 
           runHook postInstall
         '';
-        fixupPhase = ''
         # Iterate over each key-value pair in buildProperties
-        ${
-          pkgs.lib.concatStringsSep "\n" (pkgs.lib.mapAttrsToList (key: value: ''
+        # and append/update it accordingly in platform.txt
+        buildPropertiesFixupCommands = pkgs.lib.concatStringsSep "\n" (pkgs.lib.mapAttrsToList (key: value: ''
+          if grep -q "^${key}=" "$out/$dirName/platform.txt"; then
+            # If the line exists, use sed to replace it
             sed -i "s|^${key}=.*|${key}=\"${value}\"|" "$out/$dirName/platform.txt"
-          '') buildProperties)
-        }
+          else
+            # If the line doesn't exist, add it at the end of the file
+            echo "${key}=${value}" >> "$out/$dirName/platform.txt"
+          fi
+          '') buildProperties);
+        fixupPhase = ''
+          ${buildPropertiesFixupCommands}
         '';
         nativeBuildInputs = [ pkgs.unzip ];
         src = fetchurl ({
